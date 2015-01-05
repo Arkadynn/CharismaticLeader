753664a2c01d01566d4f13ad21abaee32a3f3851
Arkadynn <quentindeputier@gmail.com>
2015-01-05 11:23:49 +0100

    Debut des Personnages : 	Alien 	Barde

--- L3v1415v4/v1415v4/element/personnage/Alien.java
+++ L3v1415v4/v1415v4/element/personnage/Alien.java
@@ -1,45 +1,11 @@
 package element.personnage;
 
-import java.rmi.RemoteException;
+import element.Personnage;
 
-import element.Element;
-import element.IPersonnage;
+public class Alien extends Personnage {
 
-public class Alien extends Element implements IPersonnage {
-
-	public Alien(String nom) {
-		super(nom);
-		// TODO Auto-generated constructor stub
+	public Alien(String nom, int force, int charisme) {
+		super(nom, 20, 0);
 	}
-
-	@Override
-	public void setLeader(int ref) throws RemoteException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void clearLeader() throws RemoteException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void ajouterEquipe(int ref) throws RemoteException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void enleverEquipe(int ref) throws RemoteException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void enleverTouteEquipe() throws RemoteException {
-		// TODO Auto-generated method stub
-
-	}
-
+	
 }
---
+++ L3v1415v4/v1415v4/element/personnage/Barde.java
@@ -0,0 +1,11 @@
+package element.personnage;
+
+import element.Personnage;
+
+public class Barde extends Personnage {
+
+	public Barde(String nom, int force, int charisme) {
+		super(nom, 5, 15);
+	}
+
+}

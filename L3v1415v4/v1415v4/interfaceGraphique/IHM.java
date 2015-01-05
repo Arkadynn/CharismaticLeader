1f1d78f1b1dbafc3e817e6a08bb34c522f935551
Arkadynn <quentindeputier@gmail.com>
2015-01-05 10:28:16 +0100

    Supp du test de commit

--- L3v1415v4/v1415v4/interfaceGraphique/IHM.java
+++ L3v1415v4/v1415v4/interfaceGraphique/IHM.java
@@ -28,7 +28,6 @@ import serveur.IArene;
 /**
  * Definit l'interface graphique. Allure des fenetres, connexion, recup des elements sur l'arene et les dessines (a partir de VueElement)
  */
-// Bonjour le monde 42
 public class IHM extends JFrame {
 
 	private static final long serialVersionUID = 1L;

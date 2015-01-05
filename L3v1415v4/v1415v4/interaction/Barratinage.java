87b7a91a8e9739bd5690c8f2512aefbe1fa8bdfc
Arkadynn <quentindeputier@gmail.com>
2015-01-05 15:33:36 +0100

    Interractions 1

--- L3v1415v4/v1415v4/interaction/Barratinage.java
+++ L3v1415v4/v1415v4/interaction/Barratinage.java
@@ -3,26 +3,81 @@
  */
 package interaction;
 
-import interfaceGraphique.VueElement;
-
 import java.rmi.RemoteException;
-import java.util.Hashtable;
 
 import serveur.IArene;
+import controle.IConsole;
+import element.Personnage;
+
 
 /**
  * @author Quentin
  *
  */
-public class Barratinage extends Actions {
+public class Barratinage implements IDuel {
+	
+	/**
+	 * Arene (pour retrouver la reference du leader.
+	 */
+	private IArene arene;
 
 	/**
-	 * @param ve
-	 * @param voisins
+	 * Reference de l'attaquant.
 	 */
-	public Barratinage(VueElement ve, Hashtable<Integer, VueElement> voisins) {
-		super(ve, voisins);
+	private IConsole attaquant; 
+	/**
+	 * Reference du defenseur.
+	 */
+	private IConsole defenseur;
+	
+	public Barratinage(IArene arene, IConsole attaquant, IConsole defenseur) {
+		super();
+		this.arene = arene;
+		this.attaquant = attaquant;
+		this.defenseur = defenseur;
 	}
+
+	@Override
+	public boolean memeLeader(IConsole per1, IConsole per2)
+			throws RemoteException {
+		return ((Personnage) per1.getElement()).getLeader() != -1 
+				&& ((Personnage) per1.getElement()).getLeader() == ((Personnage) per2.getElement()).getLeader();
+	}
+
+	@Override
+	public boolean isLeader(IConsole per1, IConsole per2)
+			throws RemoteException {
+		return ((Personnage) per2.getElement()).getLeader() == per1.getRefRMI();
+	}
+
+	@Override
+	public void realiserCombat() throws RemoteException {
+		Personnage pAtt = (Personnage) attaquant.getElement();
+		Personnage pDef = (Personnage) defenseur.getElement();
 	
+		int attCharisme = pAtt.getCharisme();
+		int defCharisme = pDef.getCharisme();
 	
+		System.out.print("Duel entre " + attaquant.getRefRMI() + " et " + defenseur.getRefRMI() + " : ");
+	
+		try {
+			if(memeLeader(attaquant, defenseur) || isLeader(attaquant, defenseur)) {
+				// s'ils ont le meme leader ou que le defenseur est dans l'equipe de l'attaquant, rien ne se passe
+				System.out.println("Rien ne se passe");
+				
+			} else if (isLeader(defenseur, attaquant)) {
+				if(attCharisme > defCharisme) {
+					// coup d'etat
+					System.out.println(attaquant.getRefRMI() + " realise un coup d'etat contre " + defenseur.getRefRMI());
+				} else {
+					// coup d'etat echoue
+					// TODO Fuire
+				}
+			}
+		} catch (RemoteException e) {
+			System.out.println("Erreur lors d'un duel :");
+			e.printStackTrace();
+		}
+	}
+
 }

e8857623f4a0c5ca34347336ed3a8a3361f64da2
Geekette <alexandre.canny@gmail.com>
2015-01-05 10:44:23 +0100

    Creation d'un package potions.

---
+++ L3v1415v4/v1415v4/element/potions/Fortifiant.java
@@ -0,0 +1,12 @@
+package element.potions;
+
+import element.Element;
+
+public class Fortifiant extends Element {
+
+	public Fortifiant(String nom) {
+		super(nom);
+		// TODO Auto-generated constructor stub
+	}
+
+}

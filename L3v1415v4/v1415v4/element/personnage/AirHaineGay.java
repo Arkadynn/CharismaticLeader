73dea8ef3107af7dd693b09d26a87a5e06922fa3
Arkadynn <quentindeputier@gmail.com>
2015-01-05 13:12:48 +0100

    Et un peu d'aléatoire

---
+++ L3v1415v4/v1415v4/element/personnage/AirHaineGay.java
@@ -0,0 +1,15 @@
+package element.personnage;
+
+import java.util.Random;
+
+import element.Personnage;
+
+
+public class AirHaineGay extends Personnage {
+	 private static int rand = new Random().nextInt(300);
+	 
+	public AirHaineGay(String nom, int force, int charisme) {
+		super(nom, rand, 300 - rand);
+	}
+
+}

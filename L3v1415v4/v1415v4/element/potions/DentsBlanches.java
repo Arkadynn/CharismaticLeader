069245e896a5b9f569794b3f06bae5b12165012d
Geekette <alexandre.canny@gmail.com>
2015-01-05 13:37:10 +0100

    Ajout des potions

---
+++ L3v1415v4/v1415v4/element/potions/DentsBlanches.java
@@ -0,0 +1,18 @@
+package element.potions;
+
+import element.Potion;
+
+/**
+ * DentsBlanches est une Potion conférant un bonus de 100 en Charisme,
+ * mais infligeant un malus de 25 en force
+ * @author Alexandre CANNY
+ *
+ */
+public class DentsBlanches extends Potion {
+
+	public DentsBlanches() {
+		super("Dents Blances", -25, 100);
+		// TODO Auto-generated constructor stub
+	}
+
+}
